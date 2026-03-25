14b1d0154508d1633fe15adc51a69a3344017f51
Jerryyucode <jyu268208@gmail.com>
2026-03-24 19:07:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".site-header");
 
+  if (window.scrollY > 40) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
