cb547be291f01893c6e331f8cb6bbc9c6f926c57
Jerryyucode <jyu268208@gmail.com>
2026-03-26 14:58:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-window.addEventListener("scroll", function () {
-  const header = document.querySelector(".site-header");
+const header = document.querySelector(".site-header");
+const isSubpage = document.body.classList.contains("subpage");
 
-  if (window.scrollY > 40) {
-    header.classList.add("scrolled");
-  } else {
-    header.classList.remove("scrolled");
-  }
-});
+if (!isSubpage && header) {
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 40) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  });
+}
